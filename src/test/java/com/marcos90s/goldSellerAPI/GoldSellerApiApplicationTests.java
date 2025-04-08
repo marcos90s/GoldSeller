a783165efcc26a239e8ede5c106e625af3f15d3d
Marcos Antônio <marcosabes@hotmail.com>
2025-04-07 21:14:55 -0300

    DB Config and other stuff

---
+++ src/test/java/com/marcos90s/goldSellerAPI/GoldSellerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos90s.goldSellerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldSellerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
