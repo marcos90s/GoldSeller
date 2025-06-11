f72c59914717b7af980eeaa031177af5653c1260
Marcos Antônio <marcosabes@hotmail.com>
2025-06-11 20:41:15 -0300

    Add: More tests implementation

--- src/test/java/com/marcos90s/goldSellerAPI/UserServiceTest.java
+++ src/test/java/com/marcos90s/goldSellerAPI/UserServiceTest.java
@@ -4,6 +4,7 @@ import com.marcos90s.goldSellerAPI.dto.UsersRequestDTO;
 import com.marcos90s.goldSellerAPI.dto.UsersResponseDTO;
 import com.marcos90s.goldSellerAPI.entities.Users;
 import com.marcos90s.goldSellerAPI.enums.UserRole;
+import com.marcos90s.goldSellerAPI.exception.BadRequestException;
 import com.marcos90s.goldSellerAPI.repository.UsersRepository;
 import com.marcos90s.goldSellerAPI.services.UsersService;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +20,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -37,44 +40,51 @@ public class UserServiceTest {
     @InjectMocks
     private UsersService usersService;
 
-    private Users validUser;
+    private Users validUser1;
+    private Users validUser2;
     private UsersRequestDTO validUserRequestDTO;
-    private UsersResponseDTO validUserResponseDTO;
+    private UsersResponseDTO validUserResponseDTO1;
+    private UsersResponseDTO validUserResponseDTO2;
 
     @BeforeEach
     void setUp(){
-        validUser = new Users(null, "Joãozinho",
+        validUser1 = new Users(null, "Joãozinho",
                 "joãozinho@email.com", "senhaCodificada123",
+                UserRole.COMMON, null, null, new ArrayList<>(), new ArrayList<>());
+        validUser2 = new Users(null, "Joãozin",
+                "joãozin@email.com", "senhaCodificada123",
                 UserRole.COMMON, null, null, new ArrayList<>(), new ArrayList<>());
 
         validUserRequestDTO = new UsersRequestDTO();
-        validUserResponseDTO = new UsersResponseDTO();
+        validUserResponseDTO1 = new UsersResponseDTO();
+        validUserResponseDTO2 = new UsersResponseDTO();
 
-        validUserRequestDTO.setName(validUser.getName());
-        validUserRequestDTO.setEmail(validUser.getEmail());
-        validUserRequestDTO.setPassword("senha123");
-        validUserRequestDTO.setRole(validUser.getRole());
+        validUserRequestDTO = createUserRequestDTO(validUser1.getName(),
+                validUser1.getEmail(),
+                "senha123",
+                validUser1.getRole());
 
-        validUserResponseDTO.setId(validUser.getId());
-        validUserResponseDTO.setName(validUser.getName());
-        validUserResponseDTO.setEmail(validUser.getEmail());
-        validUserResponseDTO.setRole("COMMON");
-        validUserResponseDTO.setTotalGold(validUser.getTotalGold());
-        validUserResponseDTO.setTotalMoney(validUser.getTotalMoney());
-        validUserResponseDTO.setRealTransactionIds(new ArrayList<>());
-        validUserResponseDTO.setGameTransactionIds(new ArrayList<>());
+        validUserResponseDTO1 = createUserResponseDTO(validUser1.getId(),
+                validUser1.getName(),
+                validUser1.getEmail(),
+                "COMMON");
 
-        when(passwordEncoder.encode(validUserRequestDTO.getPassword())).thenReturn("senhaCodificada123");
+        validUserResponseDTO2 = createUserResponseDTO(validUser2.getId(),
+                validUser2.getName(), validUser2.getEmail(), "COMMON");
+
+
     }
 
     @Test
     @DisplayName("Deve cadastrar um usuário com sucesso")
     void deveCadastrarUsuarioERetornarDTOComSucesso(){
 
+        when(passwordEncoder.encode(validUserRequestDTO.getPassword())).thenReturn("senhaCodificada123");
+        //Quando a procura por email é chamada, retornamos false (cenário de sucesso).
         when(usersRepository.existsByEmail(validUserRequestDTO.getEmail())).thenReturn(false);
-
-        when(usersRepository.save(any(Users.class))).thenReturn(validUser);
-
+        //Quando salvamos qualquer Usuário do tipo User, retornamos um usuário válido (cenário de sucesso).
+        when(usersRepository.save(any(Users.class))).thenReturn(validUser1);
+        //Chamada do método de criação
         UsersResponseDTO responseDTO = usersService.createUser(validUserRequestDTO);
 
         assertNotNull(responseDTO);
@@ -82,12 +92,75 @@ public class UserServiceTest {
         assertEquals("Joãozinho", responseDTO.getName());
         assertEquals("joãozinho@email.com", responseDTO.getEmail());
         assertEquals("COMMON", responseDTO.getRole());
-        assertEquals(responseDTO.getTotalGold(), validUser.getTotalGold());
-        assertEquals(responseDTO.getTotalMoney(), validUser.getTotalMoney());
+        assertEquals(validUser1.getTotalGold(), responseDTO.getTotalGold());
+        assertEquals(validUser1.getTotalMoney(), responseDTO.getTotalMoney());
         assertEquals(new ArrayList<>(), responseDTO.getGameTransactionIds());
         assertEquals(new ArrayList<>(), responseDTO.getRealTransactionIds());
 
         verify(usersRepository, times(1)).save(any(Users.class));
 
+    }
+    @Test
+    @DisplayName("Deve lançar exceção quando email for existente")
+    void deveLancarExcecaoParaEmailExistente(){
+        //Chamar existsByEmail e retornar true;
+        when(usersRepository.existsByEmail(validUserRequestDTO.getEmail())).thenReturn(true);
+
+        BadRequestException exception = assertThrows(BadRequestException.class, () ->{
+            usersService.createUser(validUserRequestDTO);
+        });
+        assertEquals("Email is already exists!", exception.getMessage());
+        verify(usersRepository, never()).save(any(Users.class));
+    }
+
+    @Test
+    @DisplayName("Deve Retornar todos usuários como DTO")
+    void deveRetornarTodosUsersComoDTO(){
+
+        when(usersRepository.findAll()).thenReturn(Arrays.asList(validUser1, validUser2));
+
+        List<UsersResponseDTO> actualUsers = usersService.getAllUsers();
+
+        assertNotNull(actualUsers, "A Lista não deve ser nula");
+        assertEquals(2, actualUsers.size());
+        assertUserResponseDTOEquals(validUserResponseDTO1, actualUsers.get(0));
+        assertUserResponseDTOEquals(validUserResponseDTO2, actualUsers.get(1));
+
+        verify(usersRepository, times(1)).findAll();
+
+    }
+
+    private UsersResponseDTO createUserResponseDTO(String id, String name, String email, String role) {
+        UsersResponseDTO dto = new UsersResponseDTO();
+        dto.setId(id);
+        dto.setName(name);
+        dto.setEmail(email);
+        dto.setRole(role);
+        dto.setTotalGold(null);
+        dto.setTotalMoney(null);
+        dto.setRealTransactionIds(new ArrayList<>());
+        dto.setGameTransactionIds(new ArrayList<>());
+        return dto;
+    }
+
+    private UsersRequestDTO createUserRequestDTO(String name, String email, String password, UserRole role){
+        UsersRequestDTO dto = new UsersRequestDTO();
+        dto.setName(name);
+        dto.setEmail(email);
+        dto.setPassword(password);
+        dto.setRole(role);
+        return dto;
+    }
+
+    private void assertUserResponseDTOEquals(UsersResponseDTO expected, UsersResponseDTO actual){
+        assertNotNull(actual);
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getEmail(), actual.getEmail());
+        assertEquals(expected.getRole(), actual.getRole());
+        assertEquals(expected.getTotalGold(), actual.getTotalGold());
+        assertEquals(expected.getTotalMoney(), actual.getTotalMoney());
+        assertEquals(expected.getGameTransactionIds(), actual.getGameTransactionIds());
+        assertEquals(expected.getRealTransactionIds(), actual.getRealTransactionIds());
     }
 }
